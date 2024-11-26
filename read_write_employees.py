937fb0b7850e045dfc5e19135e7459d28c7fbd83
dave <dave@gmail.com>
2024-11-25 23:04:39 -0600

    deploy attempt

--- read_write_employees.py
+++ read_write_employees.py
@@ -1,13 +1,19 @@
-import json
+import json, os
 
-def load(file_name):
-    """Load the employee data from a json file."""
-    with open(file_name, 'r') as f:
-        return json.load(f)
-    
+def get_path(file_name):
+    # Get the directory where the executable/script is located
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    # Construct the full path to the JSON file
+    return os.path.join(base_dir, file_name)
+
 def update(employees, file_name):
     """Save the employee data to a json file."""
-    with open(file_name, 'w') as f:
+    file_path = get_path(file_name)
+    with open(file_path, 'w') as f:
         return json.dump(employees, f, indent=4)
     
-    
+def load(file_name):
+    """Load the employee data from a JSON file."""
+    file_path = get_path(file_name)
+    with open(file_path, 'r') as f:
+        return json.load(f)
